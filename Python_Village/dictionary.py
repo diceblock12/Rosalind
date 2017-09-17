bfd160818cddce0ff0b020cd3676b831d0936bbf
Elijah McGuire-Berk <emcguireberk@Elijahs-MacBook-Pro.local>
2017-09-17 13:58:16 -0400

    This one has an issues with the "be" word.

--- Python_Village/dictionary.py
+++ Python_Village/dictionary.py
@@ -1,4 +1,14 @@
 f = open('dictionary.txt', 'r')
+g = open('output.txt', 'w')
 words = {}
 sample_text = f.read()
 wordlist = sample_text.split(' ')
+
+for x in wordlist:
+	if x in words:
+		words[x]+=1
+	else: 
+		words[x]=1
+
+for key, value in words.items():
+	g.write(key + ' ' + str(value) + '\n') 
