024a4749822d209c7f2838e978b55354d9407876
Elijah McGuire-Berk <emcguireberk@Elijahs-MacBook-Pro.local>
2017-10-28 14:33:27 -0400

    Added in list to hold DNA nucleotides instead of a split string.

--- Bioinformatics_Stronghold/DNA_N.py
+++ Bioinformatics_Stronghold/DNA_N.py
@@ -1,12 +1,12 @@
 f = open('GATC.txt', 'r')
 sampleText = f.read()
 letters = {}
-DNASequence = sampleText.split()
+DNASequence = list(sampleText)
 for nuc in DNASequence:
 	if nuc not in letters:
 		letters[nuc] = 1
 	else:
 		letters[nuc] += 1
 
-for letter in letters.values():
-	print letter
+#formatted string: percent s each time I have a string value I want
+print '%d %d %d %d' % (letters['A'], letters['C'], letters['G'], letters['T'])
